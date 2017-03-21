30e7b9e73ab68ef98dfc1d6b598fd0316d242a5f
yael <yael@soixantecircuits.fr>
2017-03-21 11:52:55 +0100

    :art: Use node cors instead of making our own setup

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 8080
 const program = require('commander')
@@ -39,17 +40,9 @@ db.once('open', function () {
   winston.info('connection to database established')
 })
 
-// CORS middleware
-var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  next()
-}
-
+app.use(cors())
 app.use('/static', express.static(config.dataFolder))
 
-app.use(allowCrossDomain)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api/v1/medias', mediaRouter)
