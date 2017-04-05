53d4e8888c1ed1a58f71398c320fb08a724a0235
4quet <louis.fourquet@gmail.com>
2017-04-05 17:14:37 +0200

    ⬆️  Using new standard-settings

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 'use strict'
 
-const settings = require('standard-settings')()
-const nconf = require('nconf')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const server = settings.server
-const port = process.env.PORT || server.port
 
 const fs = require('fs-extra')
 const ip = require('ip')
 const winston = require('winston')
 
+// Settings
+const standardSettings = require('standard-settings')
+const nconf = require('nconf')
+const settings = nconf.get()
+const server = settings.server
+const port = process.env.PORT || server.port
+nconf.set('baseURL', 'http://' + server.host + ':' + port + '/')
+
+// Routes
 const Utils = require('./helpers/utils')
 const mediaRouter = require('./routes/media.route.js')
 const bucketRouter = require('./routes/bucket.route.js')
 const paginationRouter = require('./routes/pagination.route.js')
 
+// Mongoose
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost/media-manager')
 
@@ -28,6 +34,7 @@ db.once('open', function () {
   winston.info('connection to database established')
 })
 
+// App
 app.use(cors())
 app.use('/static', express.static(settings.folder.data))
 
@@ -42,10 +49,8 @@ app.get('/', function (req, res) {
 })
 
 app.listen(port, server.host, function () {
-  let ipAddress = ip.address()
-  winston.info('host:', ipAddress)
+  winston.info('host:', ip.address())
   winston.info('port:', port)
-  nconf.set('baseURL', 'http://' + ipAddress + ':' + port + '/')
   fs.ensureDirSync(settings.folder.data)
   if (settings.clean === true) {
     Utils.checkIntegrity()
